c4fe378ee2d782be019e096b68ad315d25d4a0aa
Pogryziony <Pogryziony98@interia.pl>
2018-01-21 23:00:26 +0100

    Poprawa Tabulacji
    poprawienie tabulacji w kwiaty.java

--- src/las/Kwiaty.java
+++ src/las/Kwiaty.java
@@ -12,44 +12,44 @@ import java.util.Random;
  * @author Student
  */
 public class Kwiaty extends Flora implements Zycie
+{
+    String gatunek;
+    String kolorplatkow;
+    boolean kolce;
+
+    public Kwiaty()
     {
-        String gatunek;
-        String kolorplatkow;
-        boolean kolce;
+        Random r = new Random();
+        switch(r.nextInt(4))
+        {
+            case 0:
+            set("Stokrotka","Biały",false);
+            break;
 
-        public Kwiaty()
-            {
-                Random r = new Random();
-                switch(r.nextInt(4))
-                    {
-                     case 0:
-                        set("Stokrotka","Biały",false);
-                        break;
-
-                    case 1:
-                        set("Dzika róza","Różowy",true);
-                        break;
+            case 1:
+            set("Dzika róza","Różowy",true);
+            break;
+                        
+            case 2:
+            set("Niezapominajka","Niebieski",false);
+            break;
                 
-                    case 2:
-                        set("Niezapominajka","Niebieski",false);
-                        break;
-                
-                    case 3:
-                        set("Zawilec","Zolty",false);
-                        break;
-                    } 
+            case 3:
+            set("Zawilec","Zolty",false);
+            break;
+        } 
            
-            }
-       @Override
-       public void show() 
-        { 
-            System.out.println("Materia: " + stanmaterii + System.lineSeparator() + "Królestwo: " + krolestwo + System.lineSeparator() +"Gatunek: " + gatunek + System.lineSeparator() + "Kolor płatków: " + kolorplatkow + System.lineSeparator() + "Czy ma kolce? " + kolce + System.lineSeparator()); 
-        }  
-       private void set( String gatunek, String kolorplatkow, Boolean kolce)
+    }
+    @Override
+    public void show() 
+    { 
+        System.out.println("Materia: " + stanmaterii + System.lineSeparator() + "Królestwo: " + krolestwo + System.lineSeparator() +"Gatunek: " + gatunek + System.lineSeparator() + "Kolor płatków: " + kolorplatkow + System.lineSeparator() + "Czy ma kolce? " + kolce + System.lineSeparator()); 
+    }  
+    private void set( String gatunek, String kolorplatkow, Boolean kolce)
     {
         this.gatunek = gatunek;
         this.kolorplatkow = kolorplatkow;
         this.kolce = kolce;
     }
-    }
+}
 
